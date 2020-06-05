ca42d24e6d63f5d2ef136fe9a38157e776d2b203
khguruge <gkasunharshana@gmail.com>
2020-06-05 13:57:36 +0530

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CrudService} from './service/crud.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularFirebase';
+
+  employee : string;
+  employeeName : string;
+  employeeAge : number;
+  employeeAddress : string;
+  message : string ;
+
+    constructor( public crudservice : CrudService){}
+
+    CreateRecord (){
+      let Record = {};
+      Record ['name'] = this.employeeName;
+      Record ['age'] = this.employeeAge;
+      Record ['address'] = this.employeeAddress;
+
+      this.crudservice.create_Newemployee(Record).then( res =>{
+        this.employeeName = "Kasun";
+        this.employeeAge = 45;
+        this.employeeAddress = "Kalubowitiyana";
+        console.log(res); 
+        this.message = "Employee data save done";
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+
 }
